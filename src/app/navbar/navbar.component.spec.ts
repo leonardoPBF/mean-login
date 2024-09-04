f3990a62e38dfad683d306f1ffeb0ae2c2c206f7
leonardoPBF <leonardobf140224@gmail.com>
2024-09-03 19:52:48 -0500

    navbar component

--- src/app/navbar/navbar.component.spec.ts
+++ src/app/navbar/navbar.component.spec.ts
@@ -1,5 +1,4 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { NavbarComponent } from './navbar.component';
 
 describe('NavbarComponent', () => {
@@ -11,7 +10,7 @@ describe('NavbarComponent', () => {
       imports: [NavbarComponent]
     })
     .compileComponents();
-    
+
     fixture = TestBed.createComponent(NavbarComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
