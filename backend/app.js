42b647413354b1d594cfde8b4c8183099864168d
leonardoPBF <leonardobf140224@gmail.com>
2024-09-08 14:25:41 -0500

    start front

--- backend/app.js
+++ backend/app.js
@@ -3,16 +3,17 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const app = express();
 const auth = require('./routes/auth');
+const path = require('path');
 
 mongoose.connect('mongodb://localhost:27017/mean-login', )
   .then(() => console.log('Conectado a MongoDB'))
   .catch(err => console.error('No se pudo conectar a MongoDB...', err));
 
 
-
 app.use(cors());
 app.use(express.json());
 app.use('/api/auth', auth);
+
 
 // Ruta de ejemplo para verificar que el servidor funciona
 app.get('/', (req, res) => res.send('API Running'));
